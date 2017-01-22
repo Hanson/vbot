29f8f59b0e29ff9f5e72f544259980da8d927c48
HanSon <h@hanc.cc>
2017-01-22 16:00:18 +0800

    增加拜年例子

---
+++ example/bainian.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: HanSon
+ * Date: 2016/12/7
+ * Time: 16:33
+ */
+
+require_once __DIR__ . './../vendor/autoload.php';
+
+use Hanson\Vbot\Foundation\Vbot;
+use Hanson\Vbot\Message\Entity\Text;
+
+$robot = new Vbot([
+    'tmp' => __DIR__ . '/./../tmp/',
+    'debug' => true
+]);
+
+$robot->server->setCustomerHandler(function () {
+    $whiteList = ['some remark name...', 'some remark name...'];
+    $blackList = ['some remark name...', 'some remark name...'];
+    contact()->each(function($item, $username) use ($whiteList, $blackList){
+        // 发送白名单
+        if($item['RemarkName'] && in_array($item['RemarkName'], $whiteList)){
+            Text::send($username, $item['RemarkName'] . ' 新年快乐');
+        }
+        // 黑名单不发送
+//        if($item['RemarkName'] && !in_array($item['RemarkName'], $blackList)){
+//            Text::send($username, $item['RemarkName'] . ' 新年快乐');
+//        }
+        // 全部人发送
+//        if($item['RemarkName']){
+//            Text::send($username, $item['RemarkName'] . ' 新年快乐');
+//        }
+    });
+    exit;
+});
+
+$robot->server->run();
