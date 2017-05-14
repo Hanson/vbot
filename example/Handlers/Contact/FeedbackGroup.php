704a7196d1018eb80e22fb9ff345d19754869b17
HanSon <h@hanc.cc>
2017-05-14 09:07:11 +0000

    Apply fixes from StyleCI

--- example/Handlers/Contact/FeedbackGroup.php
+++ example/Handlers/Contact/FeedbackGroup.php
@@ -1,8 +1,6 @@
 <?php
 
-
 namespace Hanson\Vbot\Example\Handlers\Contact;
-
 
 use Hanson\Vbot\Contact\Friends;
 use Hanson\Vbot\Contact\Groups;
@@ -12,10 +10,8 @@ use Illuminate\Support\Collection;
 
 class FeedbackGroup
 {
-
     public static function messageHandler(Collection $message, Friends $friends, Groups $groups)
     {
-
         if ($message['from']['NickName'] === 'vbot 反馈群') {
             $experience = '体验流程：
             去到 vbot 的 github 网站，clone 下来，然后 git checkout v2.0 即可。运行命令为 php example.php --session=vbot';
